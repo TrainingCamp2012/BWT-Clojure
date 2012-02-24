948fdff8f2b5c1e279d510c7d5cee357cd6b3641
yag_ays <yanagi.ayase@gmail.com>
2012-02-25 07:05:09 +0900

    add :gen-class and main function in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject BWT-Clojure "BWT-Clojure-0.0.1"
   :description "Full text search in Clojure using Burrows-Wheeler Transform"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :jvm-opts ["-Xmx3g" "-server" "-Dfile.encoding=UTF-8"]
+  :main BWT-Clojure.core)
