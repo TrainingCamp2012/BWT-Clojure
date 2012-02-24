973fd16df9101b246e3556115e66f49816b01568
yag_ays <yanagi.ayase@gmail.com>
2012-02-24 23:40:04 +0900

    initial commit

---
+++ src/BWT_Clojure/core.clj
@@ -0,0 +1,1 @@
+(ns BWT-Clojure.core)
---
+++ test/BWT_Clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns BWT-Clojure.test.core
+  (:use [BWT-Clojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
