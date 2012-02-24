7ec07f7ed233efca76a6c4df8ccf1555354d4f51
yag_ays <yanagi.ayase@gmail.com>
2012-02-24 23:46:00 +0900

    add main function(bwt-encode & bwt-decode)

---
+++ src/BWT_Clojure/bwt.clj
@@ -0,0 +1,12 @@
+
+
+(defn bwt-encode [s]
+  (map last (sort (map #(apply str %) (take (count s) (partition (count s) 1 (cycle s)))))))
+
+
+(defn bwt-decode [l]
+  (let [sl (sort #(compare (first %1) (first %2)) (map list l (iterate inc 0)))]
+    (apply str (loop [result [] n 0]
+                 (if (zero? (last (nth sl n)))
+                   (conj result (first (nth sl n)))
+                   (recur (conj result (first (nth sl n))) (last (nth sl n))))))))
