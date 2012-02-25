96289caac780652e7a28049581f0b2dc4604eb6c
yag_ays <yanagi.ayase@gmail.com>
2012-02-25 09:59:36 +0900

    delete old bwt-decode and add prep handling of query string

--- src/BWT_Clojure/bwt.clj
+++ src/BWT_Clojure/bwt.clj
@@ -1,8 +1,9 @@
 (ns BWT-Clojure.bwt
-  (:use [clojure.contrib.str-utils :only (re-gsub)]))
+  (:use [clojure.contrib.str-utils :only (re-gsub)])
+  (:use [clojure.contrib.string :only (lower-case)]))
 
 (defn text-prep [s]
-  (str (re-gsub #" " "_" s) "$"))
+  (str (re-gsub #" " "_" (lower-case s)) "$"))
 
 (defn bwt-encode [s]
   (map last (sort (map #(apply str %) (take (count s) (partition (count s) 1 (cycle s)))))))
@@ -10,20 +11,13 @@
 (defn sa [s]
   (sort #(compare (first %1) (first %2)) (map list s (iterate inc 0))))
 
-(defn bwt-decode [l]
-  (let [sl (sort #(compare (first %1) (first %2)) (map list l (iterate inc 0)))]
-    (loop [result [] n 0]
-      (if (zero? (last (nth sl n)))
-        (conj result (first (nth sl n)))
-        (recur (conj result (first (nth sl n))) (last (nth sl n)))))))
-
 (defn bwt-decode [l n]
   (let [sl (sort #(compare (first %1) (first %2)) (map list l (iterate inc 0)))
         index n]
     (loop [result [] n index]
       (if (zero? (last (nth sl n)))
         (conj result (first (nth sl n)))
-        (recur (conj result (first (nth sl n))) (last (nth sl n)))))))
+                (recur (conj result (first (nth sl n))) (last (nth sl n)))))))
 
 (defn bwt-find-index [bwt query]
   (map last (first (let [arr (sa bwt)]
@@ -39,11 +33,12 @@
                                   (conj result (filter #(= (first q) (first %)) f))))))))))
 
 (defn bwt-search [bwt query]
-  (map #(- (dec (count bwt)) (if (= (count bwt) (count %)) 0 (count %)) (count query))
-       (map #(bwt-decode bwt %) (bwt-find-index bwt query))))
+  (let [q (re-gsub #" " "_" (lower-case query))]
+    (map #(- (dec (count bwt)) (if (= (count bwt) (count %)) 0 (count %)) (count q))
+         (map #(bwt-decode bwt %) (bwt-find-index bwt q)))))
 
 
-;; (def text "abracadabra")
+;;  (def text "abracadabr a")
 
 ;; (bwt-decode (bwt-encode (text-prep text)) 0)
 ;; [\$ \a \b \r \a \c \a \d \a \b \r \a]
@@ -55,4 +50,5 @@
 ;; (0 8)
 
 ;; (bwt-search (bwt-encode (text-prep text)) "ra")
+
 ;; (9 2)
